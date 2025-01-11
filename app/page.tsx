b16f1e65c4534d996312ef523966362b3f04b03d
SEONGJUN <62754759+CastleJun@users.noreply.github.com>
2025-01-12 05:08:59 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -5,16 +5,28 @@ import { Component } from "./component";
 export default function Page() {
   return (
       <div>
-        <form
-            action={async () => {
-              "use server";
-              redirect("/한글");
-            }}
-        >
-          <button>Server redirect</button>
-        </form>
-        <Component />
-        <Link href="/한글">Link</Link>
+          <form
+              action={async () => {
+                  "use server";
+                  redirect("/한글");
+              }}
+          >
+              <button>Server redirect</button>
+          </form>
+          <Component/>
+          <Link href="/한글">Link</Link>
+
+<div>---------------------</div>
+          <form
+              action={async () => {
+                  "use server";
+                  redirect("/test");
+              }}
+          >
+              <button>Server redirect</button>
+          </form>
+          <Component/>
+          <Link href="/test">Link</Link>
       </div>
   );
 }
