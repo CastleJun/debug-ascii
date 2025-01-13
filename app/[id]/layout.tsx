8c6794f4171f8bd03dddb92cb03ee0d7d814e3c9
SEONGJUN <62754759+CastleJun@users.noreply.github.com>
2025-01-13 11:55:47 +0900

    bug

--- app/[id]/layout.tsx
+++ app/[id]/layout.tsx
@@ -2,10 +2,8 @@ import { headers } from 'next/headers';
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
     const headersList = await headers();
-    console.log('Layout Headers:');
-    for (const [key, value] of headersList.entries()) {
-        console.log(`${key}: ${value}`);
-    }
+    const matchedPath = headersList.get('x-matched-path');
+    console.log('x-matched-path:', matchedPath);
 
     return (
         <>
